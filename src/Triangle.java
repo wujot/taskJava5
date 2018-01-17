397ca8ce153f3b63577a9fb7538a5f59387d1b76
wujot <wojciech.wujot"gmail.com>
2018-01-17 17:23:16 +0100

    Following classes was created to store the values of the shape of each geometric figure: Square, Rectangle, Circle, Triangle. Class ShapeCalculator was created and contain methods to calculate area or perimeter of each geometric figure. In class ShapeCalculatorTest each geometric figure object is created, calculate area or perimeter of each geometric figure and results is printed.

---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    double sideA;
+    double sideB;
+    double sideC;
+
+    Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+}
